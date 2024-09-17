bb82405bce93509ae72a40eacb08f44795584235
lescavalcante <leticia.scav@gmail.com>
2024-09-17 23:45:40 +0000

    resolucao tempo para estudos

--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -4,3 +4,14 @@
 # 3. Crie uma variável chamada "total_horas" e, usanod o método input(), solicite a média de horas estudada por dia;
 # 4. Crie uma variável chamada "curso" e, usando o método input(), solicite o título do curso desejado;
 # 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
+
+nome = input('Qual seu nome?')
+total_dias = input ('Quantos dias por semana você estuda?')
+total_horas = input ('Quantas horas você estuda por dia?')
+curso = input ('Para qual curso você está estudando?')
+#tudo isso foi vai ser armazenado em forma de string. Precisamos converter em variável inteira
+total_dias = int(total_dias)
+total_horas = int (total_horas)
+total_semanal = total_dias*total_horas
+print (format 'Olá', (nome)', você estuda' total_dias ' por mais ou menos' total_horas 'Isso significa que você estuda' total_semanal ' para o curso de' curso)
+
